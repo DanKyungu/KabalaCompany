24dba0a96311ec2d07f2be33279fd206fb6051ed
jonathan kabala <jonathan.kabala@live.com>
2019-06-19 11:23:47 +0200

    Change to .Netstandard for DataEntity

--- KabalaCompany/KabalaCompany.DataAccess/Class1.cs
+++ KabalaCompany/KabalaCompany.DataAccess/Class1.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using KabalaCompany.DataEntity;
+using System;
 
 namespace KabalaCompany.DataAccess
 {
     public class Class1
     {
+        public void Test()
+        {
+            KabalaCompanyDbContext
+        }
     }
 }
--- KabalaCompany/KabalaCompany.DataEntity/KabalaCompanyDbContext.cs
+++ KabalaCompany/KabalaCompany.DataEntity/KabalaCompanyDbContext.cs
@@ -10,5 +10,6 @@ namespace KabalaCompany.DataEntity
 
         }
 
+
     }
 }
