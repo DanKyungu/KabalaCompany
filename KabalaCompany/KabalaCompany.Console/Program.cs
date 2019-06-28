30805242d88896b3599e9a3abe1185cd4ed7cf35
Dan Kyungu <kyungu.dan@hotmail.com>
2019-06-28 13:13:01 +0200

    Final Migration

---
+++ KabalaCompany/KabalaCompany.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using KabalaCompany.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KabalaCompany.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDataAccess employeeDataAccess = new EmployeeDataAccess();
+        }
+    }
+}
