5d934b46e68c81f98e23b11411b40c61e4d71b5e
Dan Kyungu <kyungu.dan@hotmail.com>
2019-08-30 15:00:12 +0200

    Ajout du projet KabalaCompany.Web

---
+++ KabalaCompany/KabalaCompany.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KabalaCompany.Web.Startup))]
+namespace KabalaCompany.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
